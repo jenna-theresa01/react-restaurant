0c78b27c3027955ee72c710893424597f3b0b7ff
Jenna Martin <jennamar828@gmail.com>
2023-10-12 14:13:19 -0400

    image files added

---
+++ components/organisms/MyTitle.js
@@ -0,0 +1,13 @@
+
+
+export default function TitleHome (props) {
+
+    return(
+        <div>
+            <TitleHome
+            title="Gulliver's Trifles"
+            homeTitle={setStateClickHandler}
+            />
+        </div>
+    )
+}
--- react-restaurant/app/page.js
+++ react-restaurant/app/page.js
@@ -1,11 +1,20 @@
 "use client"
 import React, { useState } from 'react'
 import styles from './page.module.css'
+import axios from 'axios'
+import bootstrap from 'bootstrap'
+// import MyTitle from './components/organisms/MyTitle'
 
 export default function Home() {
+
+  // setStateClickHandler for title of restaurant(make it go to home screen)
+function setStateClickHandler () {
+
+}
+
   return (
     <main className={styles.main}>
-
+     
     </main>
   )
 }
